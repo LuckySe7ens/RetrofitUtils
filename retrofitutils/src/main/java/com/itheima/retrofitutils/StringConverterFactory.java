3dedc93421326a385c53e4afd59e21f52ebd0338
LuckySe7ens <564892076@qq.com>
2018-11-14 12:23:53 +0800

    Update StringConverterFactory.java

--- retrofitutils/src/main/java/com/itheima/retrofitutils/StringConverterFactory.java
+++ retrofitutils/src/main/java/com/itheima/retrofitutils/StringConverterFactory.java
@@ -1,10 +1,22 @@
 package com.itheima.retrofitutils;
 
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
+import okio.Buffer;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 
@@ -16,14 +28,19 @@ public final class StringConverterFactory extends Converter.Factory {
         return INSTANCE;
     }
 
+    private final Gson gson = new Gson();
+
     // 我们只关实现从ResponseBody 到 String 的转换，所以其它方法可不覆盖
     @Override
     public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
         if (type == String.class) {
             return StringConverter.INSTANCE;
+        } else {
+            TypeAdapter<?> adapter = gson.getAdapter(TypeToken.get(type));
+            return new GsonResponseBodyConverter<>(gson, adapter);
         }
         //其它类型我们不处理，返回null就行
-        return super.responseBodyConverter(type, annotations, retrofit);
+//        return super.responseBodyConverter(type, annotations, retrofit);
     }
 }
 
@@ -36,3 +53,23 @@ class StringConverter implements Converter<ResponseBody, String> {
         return value.string();
     }
 }
+
+final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+    private final Gson gson;
+    private final TypeAdapter<T> adapter;
+
+    GsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
+    }
+
+    @Override public T convert(ResponseBody value) throws IOException {
+        JsonReader jsonReader = gson.newJsonReader(value.charStream());
+        try {
+            return adapter.read(jsonReader);
+        } finally {
+            value.close();
+        }
+    }
+}
+
